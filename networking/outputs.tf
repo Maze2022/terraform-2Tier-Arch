11ca7cf5e607d41f6225b730126d81ff564669df
Imaze Enabulele <ienabulele84@gmail.com>
2022-10-31 00:24:07 +0000

    networking/outputs.tf

---
+++ networking/outputs.tf
@@ -0,0 +1,14 @@
+# --- networking/outputs.tf ---
+
+output "vpc_id" {
+  value = aws_vpc.Week21_vpc.id
+}
+
+output "public_subnets" {
+    value = aws_subnet.lu-public_subnet.*.id
+}
+
+output "public_sg" {
+    value = aws_security_group.lu_sg.id
+}
+
