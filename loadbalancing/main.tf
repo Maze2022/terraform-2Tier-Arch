421c3db177e4d81c8a0ad4443fc1a928de530cd0
Imaze Enabulele <ienabulele84@gmail.com>
2022-10-31 00:58:16 +0000

    loadbalancing/main.tf

--- loadbalancing/main.tf
+++ loadbalancing/main.tf
@@ -1,15 +1,16 @@
 # -------- loadbalancing/main.tf
 
-# --- Target group
-resource "aws_lb_target_group" "lu-alb-TG" {
-  name     = "lu-alb-TG"
+resource "aws_lb_target_group" "lu-alb-tg" {
+  name     = "lu-alb-tg-${substr(uuid(), 0, 3)}"
   port     = var.tg_port
   protocol = var.tg_protocol
   vpc_id   = var.vpc_id
+#   lifecyle {
+#     ignore_changes = [name]
+#   }
 }
 
-# Load balancer
-resource "aws_lb" "my-aws-alb" {
+resource "aws_lb" "lu-alb" {
   name               = "lu-alb"
   internal           = false
   load_balancer_type = "application"
@@ -17,27 +18,27 @@ resource "aws_lb" "my-aws-alb" {
   subnets            = var.public_subnets
 }
 
-# # Create Load balancer listner rule
-# resource "aws_lb_listener" "lb_lst" {
-#   load_balancer_arn = aws_lb.my-aws-alb.arn
-#   port              = "80"
-#   protocol          = "HTTP"
+# Create Load balancer listner rule
+resource "aws_lb_listener" "lu-lb_lst" {
+  load_balancer_arn = aws_lb.lu-alb.arn
+  port              = var.listener_port
+  protocol          = var.listener_protocol
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.alb-TG.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lu-alb-tg.arn
+  }
+}
 
 # #Load balancer-Target group attachment
-# resource "aws_lb_target_group_attachment" "my-aws-alb" {
+# resource "aws_lb_target_group_attachment" "lu-alb" {
 #   target_group_arn = aws_lb_target_group.alb-TG.arn
 #   target_id        = aws_instance.Pub2a_ec2.id
 #   port             = 80
 # }
 
 # #Load balancer-Target group attachment
-# resource "aws_lb_target_group_attachment" "my-aws-alb2" {
+# resource "aws_lb_target_group_attachment" "lu-alb2" {
 #   target_group_arn = aws_lb_target_group.alb-TG.arn
 #   target_id        = aws_instance.Pub2b_ec2.id
 #   port             = 80
