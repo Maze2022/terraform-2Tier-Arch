6040ab428a4376bf115d77b92871057f0cda3271
Imaze Enabulele <ienabulele84+luit@gmail.com>
2022-11-01 22:05:18 +0000

    update

---
+++ compute/variables.tf
@@ -0,0 +1,23 @@
+# variable "instance_type" {
+#   type = string
+# }
+
+# variable "public_subnets" {
+#   type = string
+# }
+
+# variable "private_subnets" {
+#   type = string
+# }
+
+# variable "bastion_host_sg" {
+#   type = string
+# }
+
+# variable "webserver_sg" {
+#   type = string
+# }
+
+# variable "key_name" {
+#   type = string
+# }
