cddb23b1298419e3117bb22d03f20f882af4a0ce
Imaze Enabulele <ienabulele84+luit@gmail.com>
2022-11-02 16:42:55 +0000

    update

--- compute/variables.tf
+++ compute/variables.tf
@@ -1,23 +1,15 @@
-# variable "instance_type" {
-#   type = string
-# }
+variable "instance_type" {}
 
-# variable "public_subnets" {
-#   type = string
-# }
+variable "public_subnets" {}
 
-# variable "private_subnets" {
-#   type = string
-# }
+variable "private_subnets" {}
 
-# variable "bastion_host_sg" {
-#   type = string
-# }
+variable "bastion_host_sg" {}
 
-# variable "webserver_sg" {
-#   type = string
-# }
+variable "webserver_sg" {}
 
-# variable "key_name" {
-#   type = string
-# }
+variable "key_name" {}
+
+variable "user_data" {}
+
+variable "lb_tg" {}
