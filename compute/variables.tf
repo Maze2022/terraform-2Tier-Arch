cc24b1aae0225dd2f1ce2060a8475749d30d4929
Maze2022 <108425175+Maze2022@users.noreply.github.com>
2022-11-05 09:51:10 -0600

    Final commit

--- compute/variables.tf
+++ compute/variables.tf
@@ -4,6 +4,5 @@ variable "private_subnets" {}
 variable "bastion_host_sg" {}
 variable "webserver_sg" {}
 variable "key_name" {}
-# variable "public_key_path" {}
 variable "user_data" {}
 variable "lb_tg" {}
